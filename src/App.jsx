f83de963071cfa8018dcf9a6b33bccfffaf47890
Guilherme <guilhermesilba811@gmail.com>
2023-11-24 16:50:00 -0300

    Desafio 01 resolvido

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,24 @@
-const App = () => <h1>Hello, React</h1>
+const App = () => (
+  <div className="container">
+    <div className="count">
+      <button onClick={() => console.log("clicou no menos do intervalo")}>
+        -
+      </button>
+      <p>Intervalo:1 </p>
+      <button onClick={() => console.log("clicou no mais do intervalo")}>
+        +
+      </button>
+    </div>
+    <div className="count">
+      <button onClick={() => console.log("clicou no menos da contagem")}>
+        -
+      </button>
+      <p>Contagem:0 </p>
+      <button onClick={() => console.log("clicou no mais da contagem")}>
+        +
+      </button>
+    </div>
+  </div>
+);
 
-export{ App };
+export { App };
