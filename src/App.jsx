c715d56d33bf252eacb70146e89f6d558e3a182a
Guilherme <guilhermesilba811@gmail.com>
2023-11-26 16:33:08 -0300

    desafio 04

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,40 @@
-const cardsInfo = [
+const faqs = [
   {
-    id: 7336,
-    question: "Do quê aplicações React são feitas?",
-    answer: "Componentes",
+    id: crypto.randomUUID(),
+    title: "Quem é a maior celebridade do mundo dos games no BR?",
+    answer: "A apresentadora Ana Maria Braga. Joga 3h pra desocupar a cabeça.",
+    number: 1,
   },
   {
-    id: 8832,
-    question: "Qual é o nome da sintaxe usada para descrever UI no React?",
-    answer: "JSX",
+    id: crypto.randomUUID(),
+    title: "Onde fica a europa da américa do sul?",
+    answer: "Argentina. BR vive como rei em Buenos Aires.",
+    number: 2,
   },
   {
-    id: 3457,
-    question: "Em qual linguagem o React é baseado?",
-    answer: "JavaScript",
-  },
-  {
-    id: 9103,
-    question: "Qual método usar para renderizar uma lista no React?",
-    answer: "map",
-  },
-  {
-    id: 1297,
-    question: "Qual empresa criou o React?",
-    answer: "Meta",
-  },
-  {
-    id: 2002,
-    question: "Como passar informações para um componente React?",
-    answer: "Através de props",
+    id: crypto.randomUUID(),
+    title: "Qual é a grande oportunidade para desenvolvedores BR?",
+    answer: "Aproveitar a alta demanda pra ganhar em dólar ou euro.",
+    number: 3,
   },
 ];
 
-/* const getIds = cardsInfo.map((infoId) => infoId.id); */
-
 const App = () => {
-  /* const showId = () => {
-    console.log(getIds[0]);
-  }; */
   return (
-    <>
-      <div className="flashcards">
-        <ul style={{ listStyle: "none" }}>
-          {cardsInfo.map((info) => (
-            <li onClick={() => console.log(info.id)} key={info.id}>
-              <h4 className="card">{info.question}</h4>
+    <div className="accordion">
+      <ul>
+        {faqs.map((item) => {
+          return (
+            <li className="item " key={item.id}>
+              <p className="number">{item.number}</p>
+              {item.title}
+              <p className="answer">{item.answer}</p>
+              <p className="icon">-</p>
             </li>
-          ))}
-        </ul>
-      </div>
-    </>
+          );
+        })}
+      </ul>
+    </div>
   );
 };
 
